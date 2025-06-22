48238f76c7f5bdd90b7c529d4f1fa995be9c91a9
Nisugi <nisugi.gs4@gmail.com>
2025-06-22 16:47:24 -0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # Set to true to repost new messages from the database
 REPLAY_MODE = False
-FULL_BACKFILL_RUN = True
+FULL_BACKFILL_RUN = False
 
 TOKEN         = os.getenv("DISCORD_TOKEN")
 GITHUB_TOKEN  = os.getenv("GITHUB_TOKEN")
